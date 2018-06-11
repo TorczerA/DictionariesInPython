b1babea631416804abe6c2766c13b7d8723f81b0
TorczerA <coolrohit1996@gmail.com>
2018-06-12 03:19:21 +0530

    Initial commit

---
+++ dictionaries_in_python.py
@@ -0,0 +1,20 @@
+#Dictionaries are Key-Value pairs.
+
+print("Dictionaries are Key-Value pairs.")
+
+d = {
+    'word':'definition',
+    'word2':'definition2',
+    'word3' : 'definition3'
+}
+
+a = {
+    'a':43,
+    'b':56,
+    'c':89
+}
+
+print("d =",d)
+print("a =",a)
+print("a['b'] =",a['b'])
+print("d['word3'] =",d['word3'])
